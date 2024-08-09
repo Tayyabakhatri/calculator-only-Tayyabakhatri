4229a68928e2f9190b9434c36acbbadc73707e0e
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-09 09:27:15 +0500

    modified

--- app.js
+++ app.js
@@ -16,26 +16,61 @@ function del() {
 }
 
 var memory = 0;
-function a() {
-  var currentValue = parseFloat(document.getElementById("data"));
-  var val = currentValue.value;
-  memory += val;
-  val = memory;
+function memoryPlus() {
+  var currentValue = parseFloat(document.getElementById("data").value);
+  memory += currentValue;
+  document.getElementById("data").value = memory;
 }
-function clear() {
-  var inputData = document.getElementById("data");
-  inputData.value = "";
+function memoryMinus() {
+  var currentValue = parseFloat(document.getElementById("data").value);
+  memory -= currentValue;
+  document.getElementById("data").value = memory;
 }
-document.querySelectorAll('.btn-1').forEach((div)=>{
-    div.addEventListener('click',()=>{
-        speak(div.textContent)
-    })
-})
-// var synth= window.speechSynthesis
+function memoryClear() {
+  document.getElementById("data").value = "";
+  memory = 0;
+}
+function clearData() {
+  document.getElementById("data").value = "";
+}
 
-// function speak (text){
-//     var utterance = newSpeachSynthesisUtterance(text);
-//     SpeechSynthesis.speak(utterance)
-// }
-// var a = "hellow world"
-// speak(a)
+function speak(text) {
+  if (isReadAloudEnable) {
+    const speech = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(speech);
+  }
+}
+
+document.querySelectorAll(".btn-1").forEach((div) => {
+  div.addEventListener("click", () => {
+    speak(div.textContent);
+  });
+});
+
+var volumeOnIcon = document.getElementById("volume-on");
+var volumeOffIcon = document.getElementById("volume-off");
+var isReadAloudEnable = false;
+
+function enableReadAloud() {
+  isReadAloudEnable = true;
+  console.log("Read-aloud enabled");
+}
+
+function disableReadAloud() {
+  isReadAloudEnable = false;
+  console.log("Read-aloud disabled");
+  window.speechSynthesis.cancel(); // Stop any ongoing speech
+}
+
+volumeOnIcon.addEventListener("click", () => {
+  enableReadAloud();
+  volumeOnIcon.style.color = "green";
+  volumeOffIcon.style.color = "#67656a";
+});
+
+volumeOffIcon.addEventListener("click", () => {
+  disableReadAloud();
+  volumeOffIcon.style.color = "red";
+  volumeOnIcon.style.color = "#67656a";
+});
+
